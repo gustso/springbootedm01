bf9743756191d1b176c259cf791c7b2bc91b2da3
NTGustso <NTGustso@DESKTOP-0IA820K>
2020-03-12 07:48:22 -0700

    primer commit

---
+++ springbootclase04/src/main/java/unju/fi/edm/Springbootclase04Application.java
@@ -0,0 +1,13 @@
+package unju.fi.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootclase04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootclase04Application.class, args);
+	}
+
+}
---
+++ springbootclase04/src/test/java/unju/fi/edm/Springbootclase04ApplicationTests.java
@@ -0,0 +1,13 @@
+package unju.fi.edm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootclase04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
